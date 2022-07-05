8ba915e8ae118722eb18feb1b0b7ece23d7f793b
v1nam <vinamraj01@gmail.com>
2022-07-05 16:40:47 +0530

    add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -30,21 +30,32 @@ Widget build(BuildContext context) {
       appBar: AppBar(
         title: const Text(title),
       ),
-      body: ListView.builder(
+      floatingActionButton: FloatingActionButton.small(onPressed: () {}, child: const Icon(Icons.add),),
+      body: items.isNotEmpty ? ListView.builder(
         padding: const EdgeInsets.all(20),
         itemCount: items.length,
         itemBuilder: (context, index) {
           final item = items[index];
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              item.buildDate(),
-              item.buildTime(),
-              Text("Testtttttttttttttttttttttttttttttttt $index", style: const TextStyle(fontSize: 16)),
-              item.buildTrailing()
-            ],
-          );
-        }),
+          return Container(
+            padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(children: [
+                  item.buildDate(),
+                  const SizedBox.square(dimension: 30),
+                  item.buildTime()]),
+                const SizedBox.square(dimension: 50),
+                Flexible(child: Text("this is some text that i am writing randomly to fill up space for testing overflow on bigger window and i dont want random nonsense i mean this is random nonsense but like its readable i am just bored my homework shit aaa $index", style: const TextStyle(fontSize: 16), overflow: TextOverflow.ellipsis,)),
+                const SizedBox.square(dimension: 50),
+                Row(children: [
+                  item.buildEditBtn(),
+                  const SizedBox.square(dimension: 10),
+                  item.buildDeleteBtn()
+                ])
+              ],)
+            );
+          }) : const Center(child: Text("You don't have any entries", style: TextStyle(fontSize: 32, color: Colors.blueGrey, fontWeight: FontWeight.w300, fontStyle: FontStyle.italic))),
       ),
     );
   }
@@ -74,9 +85,41 @@ class ListItem {
     return Text(formatTime(date), style: const TextStyle(fontSize: 16),);
   }
 
-  Widget buildTrailing() {
-    return const Icon(Icons.delete);
+  Widget buildDeleteBtn() {
+    return ImgButton(action: () {}, img: const Icon(Icons.delete, color: Colors.red), padding: const EdgeInsets.all(12), color: Colors.red);
+  }
+
+  Widget buildEditBtn() {
+    return ImgButton(action: () {}, img: const Icon(Icons.edit), padding: const EdgeInsets.all(12));
   }
 }
 
+class ImgButton extends StatelessWidget {
+  const ImgButton(
+      {Key? key,
+      required this.action,
+      required this.img,
+      required this.padding,
+      this.color = Colors.blue})
+      : super(key: key);
 
+  final void Function() action;
+  final Widget img;
+  final EdgeInsets padding;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+        onPressed: action,
+        style: ButtonStyle(
+          side: MaterialStateProperty.resolveWith<BorderSide>((states) =>
+              BorderSide(
+                  color: states.contains(MaterialState.hovered) ? color : Colors.blueGrey,
+                  width: 2)),
+          padding: MaterialStateProperty.all(padding),
+          overlayColor: MaterialStateProperty.all(color.withAlpha(40)),
+        ),
+        child: img);
+  }
+}
